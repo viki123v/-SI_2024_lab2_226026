08b575e7d1469ce1928f4d746c3fe515273293d7
Viki123V <viktorhristovski629@gmail.com>
2024-05-24 20:34:03 +0200

    feat: Finish project

--- app/src/test/java/org/example/SILabTest.java
+++ app/src/test/java/org/example/SILabTest.java
@@ -9,32 +9,26 @@ public class SILabTest{
 
     @Test
     public void everyBranch(){
-        /**
-         * - allItems=null, payment = 0
-         * - allItems=({discount=10,price=301,barcode=0,name=a}), payment=300
-         * - allitems=({name=a,barcode=123,price=300,disc=0}), payment = 200
-         * - allitems=({bardoce=ab,price=0,discount=0,name=a}), payment = 0
-         * - allitems=({barcode=null,price=0,discount=0,name=""}), payment = 0
-         * */
+        /*
+            - allItems=null, payment = 0
+            - allItems=({discount=10,price=301,barcode=0,name=a}), payment=300
+            - allitems=({name=a,barcode=123,price=300,disc=0}), payment = 200
+            - allitems=({bardoce=ab,price=0,discount=0,name=a}), payment = 0
+            - allitems=({barcode=null,price=0,discount=0,name=""}), payment = 0
+        * */
 
-        assertThrows(RuntimeException.class,() -> SILab2.checkCart(null, 0));
-        assertThrows(RuntimeException.class,() -> SILab2.checkCart(List.of(
-                new Item("",null,0,0)
-        ), 0));
-        assertThrows(RuntimeException.class, () -> SILab2.checkCart(List.of(
-                new Item("a","ab",0,0)
-        ),0));
-        assertFalse(SILab2.checkCart(List.of(
-                new Item("a","123",300,(float) 0.0)
-        ),200));
-        assertTrue(SILab2.checkCart(List.of(
-                new Item("a","0",301,(float) 0.1)
-        ),300));
+       assertThrows(RuntimeException.class, () -> SILab2.checkCart(null, 0)) ;
+
+       assertTrue(SILab2.checkCart(List.of(new Item("a","0",301,(float) 0.1)),300));
+       assertFalse(SILab2.checkCart(List.of(new Item("a","123",300,0)),200));
+
+       assertThrows(RuntimeException.class, () -> SILab2.checkCart(List.of(new Item("a","ab",0,0)), 0));
+       assertThrows(RuntimeException.class, () -> SILab2.checkCart(List.of(new Item("",null,0,0)),0));
     }
 
     @Test 
     public void mulitpleMethods(){
-        /**
+        /*
          * allItems=(
          *     {price=100,barcode=1,name=a,discount=0}, sum = 100
          *     {price=301,discount=0,barcode=1,name=a}, sum = 301
